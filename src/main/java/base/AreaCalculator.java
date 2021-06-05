7251c5757f38ae8dd586eb8219278d7a11a75c4c
Arya Hirode <aryahirode2001@gmail.com>
2021-06-05 13:08:00 -0400

    Initial commit

---
+++ src/main/java/base/AreaCalculator.java
@@ -0,0 +1,14 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Arya Hirode
+ */
+package base;
+
+public class AreaCalculator {
+    final double f2m2Conversion = 0.09290304;
+    public static void main(String[] args) {
+
+    }
+
+    
+}
